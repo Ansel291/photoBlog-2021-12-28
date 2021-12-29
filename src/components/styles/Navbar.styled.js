865eab8f09096a021b9f8768f052ebb69ddb61b6
jason <jason@herman-scheer.com>
2021-12-29 15:42:20 -0800

    fixing mobile alignment

--- src/components/styles/Navbar.styled.js
+++ src/components/styles/Navbar.styled.js
@@ -64,7 +64,7 @@ export const SiYourtraveldottv = styled.div`
 
 export const MobileIcon = styled.div`
     position: absolute;
-    top: 1.25px;
+    top: 3px;
     right: -3px;
     transform: translate(-100%, 60%);
     font-size 1.6rem;
