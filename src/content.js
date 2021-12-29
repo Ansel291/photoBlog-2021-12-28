31dc4aca5c6eb5d53a91125f6cc22e58ed9d1ad2
jason <jason@herman-scheer.com>
2021-12-28 16:15:22 -0800

    trying to debug video autoplay

--- src/content.js
+++ src/content.js
@@ -8,24 +8,24 @@ const content = [
     },
     {
       id: 2,
-      title: 'Punta Cana, Dominican Republic',
-      date: '07/15/21',
-      body: 'The deck along the powdery white beach of the all-inclusive Hyatt Zilara Resort.  The calm waters are perfect for learning to sail on a catamaran.',
-      image: 'zilaraPuntaCanaDockOpt.png',
-    },
-    {
-      id: 3,
       title: 'Lake Tahoe - Edgewood Resort Sunset',
-      date: '03/14/21',
+      date: '11/30/21',
       body: 'Another view from Edgewood Resort in South Lake Tahoe as the sun has a chance to break through the clouds during sunset.  The views were gorgeous.',
       image: 'tahoeEdgewoodGoldOpt.png',
     },
     {
-      id: 4,
+      id: 3,
       title: 'Hawaii - Waimanolo Beach',
-      date: '01/22/21',
+      date: '07/15/21',
       body: "Located on the southeast side of Oahu, Waimanolo is a local's beach.  The blue waters are pristine, the breezes are constant, and the gold-colored sand is powdery soft.",
       image: 'hawaiiWaimanoloBeachOpt.png',
+    },
+    {
+      id: 4,
+      title: 'Punta Cana, Dominican Republic',
+      date: '05/23/21',
+      body: 'The deck along the powdery white beach of the all-inclusive Hyatt Zilara Resort.  The calm waters are perfect for learning to sail on a catamaran.',
+      image: 'zilaraPuntaCanaDockOpt.png',
     },
   ]
   
