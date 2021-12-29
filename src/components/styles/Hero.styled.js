86087d28af5b02210b469c5e6739a7cd214df857
jason <jason@herman-scheer.com>
2021-12-29 11:14:15 -0800

    removed unneeded padding from hero

--- src/components/styles/Hero.styled.js
+++ src/components/styles/Hero.styled.js
@@ -6,7 +6,6 @@ export const HeroContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    padding: 0 30px;
     height: 600px;
     position: relative;
     z-index: 1;
